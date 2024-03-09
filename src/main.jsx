3c0ba8cd2dffa270a949ee7e14cb94987620d342
sepan mohammed <sepanmustafa@gmail.com>
2024-03-09 16:51:16 -0600

    added react router dom to the package.json and created a homepage.jsx and routed the home as the first page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import './index.css'
 
+import Home from "./pages/homepage.jsx"
+import App from "./App.jsx";
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      }
+    ]
+  }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
